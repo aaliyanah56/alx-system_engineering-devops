2f4b86924d7067cd42a86d9685a8224b2008b484
aaliyanah56 <rahmaadannoor@gmail.com>
2022-09-16 16:10:21 +0300

    0x0A-management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs puppet-lint, version 2.1.1
-
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Installing flask 
+package { 'flask':
+  ensure   => '2.1.0',
+  provider =>  'pip3',
 }
